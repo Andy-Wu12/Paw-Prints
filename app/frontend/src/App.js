17295988d71f0ccafa1cbabe49aef50a1f2b78ba
Andy Wu <awpats12@gmail.com>
2022-08-25 18:27:11 -0400

    Made query form its own component

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -4,13 +4,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <DogQueryForm />
+    </div>
+  );
+}
+
+function DogQueryForm() {
+  return (
+    <>
       <h1>Lots of dogs! 🐕</h1>
       <p>See photos of your favorite dogs</p>
       <form>
         <input type="text" name="breed" placeholder="Enter a dog breed"/>
         <button type="submit">Fetch</button>
       </form>
-    </div>
+    </>
   );
 }
 
