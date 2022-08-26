d412da4f6716f252c453c58d2972ddd94ff0494b
Andy Wu <awpats12@gmail.com>
2022-08-26 18:38:20 -0400

    Getting images by form query now working (#3)

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -6,28 +6,49 @@ import './styles/style.css';
 function App() {
   return (
     <div className="App">
-      <DogQueryForm />
-      <br/>
-      <RandomDogImage />
+      <section id='QueryBreedSection'>
+        <DogQueryForm />
+      </section>
+      <section id='RandomImage'>
+        {/* <RandomDogImage /> */}
+      </section>
     </div>
   );
 }
 
 function DogQueryForm() {
+  const [imageLinks, setImageLinks] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const breed = e.target.breed.value.trim();
+    console.log(e);
+    
+    if(breed !== undefined && breed.length > 0) {
+      fetch(`http://localhost:3011/dog/${breed}/get-images`)
+      .then(response => response.json())
+      .then(data => setImageLinks(data['message']));
+    }
+  }
+
   return (
-    <div>
+    <div className='query-form'>
       <h1>Lots of dogs! 🐕</h1>
       <p>See photos of your favorite dogs</p>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input type="text" name="breed" placeholder="Enter a dog breed"/>
         <button type="submit">Fetch</button>
       </form>
+      <br/>
+      <ImageList images={imageLinks} />
+
     </div>
   );
 }
 
+
 function RandomDogImage() {
-  const [imageLink, setImageLink] = useState([]);
+  const [imageLink, setImageLink] = useState('');
 
   useEffect(() => {
     fetch('http://localhost:3011/dog/get-random')
@@ -37,18 +58,18 @@ function RandomDogImage() {
   }, []);
 
   return (
-    <div>
+    <div className='random-image-container'>
       <h1> Random Dog Image </h1>
       <p> See a new photo every time you reload the page! </p>
-      <img className='dog-image' src={imageLink} />
+      <img className='dog-image' src={imageLink} alt='Dog' />
     </div>
   );
 }
 
 function ImageList(props) {
-  const imageLinks = props.images
-  const imageList = imageLinks.map((link) =>
-    <img className='dog-image' src={link} />
+  const imageLinks = props.images;
+  const imageList = imageLinks.map((link, i) =>
+    <img key={`image${i}`} className='dog-image' src={link} alt='Dog' />
   );
 
   return (
