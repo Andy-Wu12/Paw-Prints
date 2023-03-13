e653d45810c39cd5e34f2e16bb2a26d0fabb9b10
Andy Wu <awpats12@gmail.com>
2023-03-13 18:03:36 -0400

    Fixed 404 page issues - all tests pass

--- frontend/src/routes/404.tsx
+++ frontend/src/routes/404.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import video404 from '../assets/404_Video.mp4';
 
 export default function NotFound() {
+  
+  useEffect(() => {
+    document.title = "Not Found";
+  }, [])
+
   return (
     <div>
-      <h1> W-What are you doing here? (404) </h1>
+      <h1> W-What are you doing here? (ERROR 404) </h1>
+      <h2> The URL you entered does not exist </h2>
       <video width="90%" style={{maxHeight: "80vh"}} autoPlay loop muted>
         <source src={video404} type="video/mp4" />
         Your browser does not support videos!
