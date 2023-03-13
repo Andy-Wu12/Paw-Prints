c66294a218ae6ffdc3b0166056645ec85de3db93
Andy Wu <awpats12@gmail.com>
2023-03-12 23:44:47 -0400

    Setup testing with jest, wrote basic tests for Homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,14 +8,13 @@ import './styles/likableImage.css';
 import { RandomDogImage } from './routes/RandomDogImage.tsx';
 import { QueryBreedSection } from './routes/QueryBreedSection.tsx';
 import Error404 from './routes/404.tsx';
+import Home from './routes/Home';
 import FavoriteImages from './routes/Favorites.tsx';
 import NavMenu from './components/NavMenu.tsx';
 
 // Material UI
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import Link from '@mui/material/Link';
-import GitHubIcon from '@mui/icons-material/GitHub';
 
 const lightTheme = createTheme({
   palette: {
@@ -55,21 +54,6 @@ function App() {
         
       </div>
     </ThemeProvider>
-  );
-}
-
-function Home() {
-  return (
-    <>
-      <h1>View Some Dogs</h1>
-      <p> 
-        This website uses <Link href="https://dog.ceo/dog-api/" target="_blank" rel='noopener noreferrer'>Dog API</Link>,
-        a free REST API that provides access to a large collection of open source dog pictures.
-      </p>
-      <GitHubIcon/> <Link href="https://github.com/Andy-Wu12/View-Some-Dogs">View on GitHub</Link>
-      <h2> Open the menu in the top left to get started. </h2>
-      
-    </>
   );
 }
 
